c1f677418eda839c6ac81f7099fafea0a48a3e19
himanishpuri <himanishpuri2203@gmail.com>
2025-04-07 11:50:49 +0530

    changes

--- server/src/Configurations/db.config.js
+++ server/src/Configurations/db.config.js
@@ -2,7 +2,9 @@ import { connect } from "mongoose";
 
 const ConnectDB = async function () {
 	try {
-		const dbRes = await connect(process.env.MONGO_URI);
+		const dbRes = await connect(process.env.MONGO_URI, {
+			dbName: "Chime",
+		});
 		if (dbRes) console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB:", error);
